b12f12c54961127d5a4eaf1349050dd2568c1d85
app-stack <225businesses@gmail.com>
2023-07-04 17:02:05 +0200

    sauv_4_7_2023

--- MySQL/SQL/chocoblast.sql
+++ MySQL/SQL/chocoblast.sql
@@ -1,63 +1,62 @@
 create database choco;
 use choco;
 
-create table tb_utilisateur (
-id_utilisateur int primary key auto_increment not null ,
-name_utilisateur varchar (50),
-first_name_utilisateur varchar (50),
-email_utilisateur varchar (100),
-password_utilisateur varchar (100),
-image_utilisateur varchar (100),
-status_utilisateur tinyint,
+create table roles(
+id_roles int primary key auto_increment not null,
+nom varchar(50) not null
+)Engine=InnoDB;
+
+create table utilisateur(
+id_utilisateur int primary key auto_increment not null,
+nom varchar(50) not null,
+prenom varchar(50) not null,
+email varchar(50) not null,
+password varchar(100) not null,
+image varchar(50) not null,
+statut tinyint(1) default 0,
 id_roles int not null
-) Engine=InnoDB;
+)Engine=InnoDB;
 
-create table tb_commentaire (
-id_commentaire int primary key auto_increment not null ,
-note_commentaire int,
-contenu_commentaire varchar (50),
-date_creation datetime,
-status tinyint,
-id_chocoblast int not null,
-auteur int not null
-) Engine=InnoDB;
-
-create table tb_chocoblast (
-id_chocoblast int primary key auto_increment not null ,
-slogan_chocoblast text,
-date_creation datetime,
-status tinyint,
+create table chocoblast(
+id_chocoblast int primary key auto_increment not null,
+slogan text not null,
+date_creation date not null,
+statut tinyint(1) default 0,
 auteur int not null,
 cible int not null
-) Engine=InnoDB;
+)Engine=InnoDB;
 
-create table tb_roles (
-id_roles int primary key auto_increment not null,
-name_roles varchar(50)
-) Engine=InnoDB;
+create table commentaire(
+id_commentaire int primary key auto_increment not null,
+note int not null,
+contenu text not null,
+date_creation datetime not null,
+statut tinyint(1) default 0,
+id_chocoblast int not null,
+auteur int not null
+)Engine=InnoDB;
 
-
-alter table tb_utilisateur
+alter table utilisateur
 add constraint fk_posseder_roles
 foreign key(id_roles)
-references tb_roles (id_roles);
+references roles(id_roles);
 
-alter table tb_chocoblast
+alter table chocoblast
+add constraint fk_chocoblaster_utilisateur
+foreign key(auteur)
+references utilisateur(id_utilisateur);
+
+alter table chocoblast
 add constraint fk_cibler_utilisateur
 foreign key(cible)
-references tb_utilisateur (id_utilisateur);
+references utilisateur(id_utilisateur);
 
-alter table tb_chocoblast
-add constraint fk_effectuer_utilisateur
+alter table commentaire
+add constraint fk_poster_utilisateur
 foreign key(auteur)
-references tb_utilisateur (id_utilisateur);
+references utilisateur(id_utilisateur);
 
-alter table tb_commentaire
-add constraint fk_rediger_utilisateur
-foreign key(auteur)
-references tb_utilisateur (id_utilisateur);
-
-alter table tb_commentaire
-add constraint fk_commenter_utilisateur
-foreign key(auteur)
-references tb_chocoblast (id_chocoblast);
+alter table commentaire
+add constraint fk_rattacher_chocoblast
+foreign key(id_chocoblast)
+references chocoblast(id_chocoblast);
