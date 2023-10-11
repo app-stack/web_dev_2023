7891f029e287544bbd3ffd1802fc9d12ea6d47b4
app-stack <225businesses@gmail.com>
2023-10-11 11:09:58 +0200

    11-10-2023_11h_09

--- CMS/xampp_disk_dur/xampp/htdocs/MVC_UTIL/utils/connectBdd.php
+++
@@ -1,5 +0,0 @@
-<?php
-    //fichier de connexion à la BDD
-    $bdd = new PDO('mysql:host=localhost;dbname=utilisateurs', 'root','', 
-    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-?>
--- CMS/xampp_disk_dur/xampp/htdocs/php_mvc/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Location:./controler/controler_article.php');
-?>
